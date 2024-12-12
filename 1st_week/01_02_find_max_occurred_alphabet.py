f321a6c1ee0ef72576e92d2abdf149a148dae4e2
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-12-12 13:54:34 +0900

    커밋테스트

---
+++ 1st_week/01_02_find_max_occurred_alphabet.py
@@ -0,0 +1,13 @@
+# 두번째 문제 : 최빈값 찾기 (프로그래머스 : https://school.programmers.co.kr/learn/courses/30/lessons/120812)
+# Q. Q.  다음과 같은 문자열을 입력받았을 때, 어떤 알파벳이 가장 많이 포함되어 있는지 반환하시오
+# "hello my name is dingcodingco"
+
+def find_max_occurred_alphabet(string):
+    # 이 부분을 채워보세요!
+    return "a"
+
+
+result = find_max_occurred_alphabet
+print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
+print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
+print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
