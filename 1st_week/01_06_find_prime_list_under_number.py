96cbdd2b827567f0cea6dbe8f99ca19d0f7cff0a
jaemyungchoi95 <woaud130@gmail.com>
2025-08-09 11:38:07 +0900

    restart

--- 1st_week/01_06_find_prime_list_under_number.py
+++ 1st_week/01_06_find_prime_list_under_number.py
@@ -1,11 +1,18 @@
-a = input().split()
-m = int(a[0])
-n = int(a[1])
+input = 20
 
-for i in range(m, n+1):
-    temp = 0
-    for j in range(1, i+1):
-        if i%j == 0:
-            temp += 1
-    if temp == 2:
-        print(i)
+# 소수는 자기 자신과 1 외에는 아무것도 나눌 수 없다.
+def find_prime_list_under_number(number):
+    prime_list = []
+
+    # 2 ~ 20찾아서 이것들이 소수인가? 소수라면 prime_list 에 넣어라!
+    for n in range(2, number + 1): # 2~n 까지의 숫자들이 n에 들어가는 것을 반복한다
+        for i in range(2, n): # 2 부터 n - 1 까지를 i 에 들어가는 것을 반복한다
+            if n % i == 0: # ddd
+                break
+        else:
+            prime_list.append(n)
+
+    return []
+
+result = find_prime_list_under_number(input)
+print(result)
